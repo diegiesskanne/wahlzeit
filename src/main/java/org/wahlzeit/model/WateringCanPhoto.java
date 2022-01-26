fd8de05a0e2052c8dcb985371f9eb5ec67e7d358
diegiesskanne <jaromir.vogt@web.de>
2022-01-26 12:25:04 +0100

    adap-cw12 remove color

--- src/main/java/org/wahlzeit/model/WateringCanPhoto.java
+++ src/main/java/org/wahlzeit/model/WateringCanPhoto.java
@@ -20,8 +20,6 @@ public class WateringCanPhoto extends Photo {
 
     public WateringCan watering_can;
 
-    private Color color;
-
     public WateringCanPhotoManager manager;
 
     /**
@@ -46,7 +44,6 @@ public class WateringCanPhoto extends Photo {
 
     public WateringCanPhoto(Color color){
         super();
-        this.color = color;
     }
 
     /**
@@ -58,37 +55,8 @@ public class WateringCanPhoto extends Photo {
         super(rset);
     }
 
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(String scolor){
-
-        Color color;
-        try {
-            Field field = Color.class.getField(scolor);
-            color = (Color)field.get(null);
-        } catch (Exception e) {
-            color = null; // Not defined
-            throw new NullPointerException("color is null");
-        }
-        this.color = color;
-
-    }
-
     public WateringCanPhotoManager getManager(){
         return manager;
     }
 
-    @Override
-    public void writeOn(ResultSet resultSet) throws SQLException {
-        super.writeOn(resultSet);
-        resultSet.updateInt("color", Integer.parseInt(this.getColor().toString()));
-    }
-
-    @Override
-    public void readFrom(ResultSet resultSet) throws SQLException {
-        super.readFrom(resultSet);
-        this.setColor(resultSet.getString("color"));
-    }
 }
